1a0fe47714098aaf0bbe3e0b265c65c21eb0d74a
scottkoons <scottkoons@gmail.com>
2021-08-25 16:33:57 -0600

    End Module 4

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AppHeader, Home, Host, Listing, Listings, Login, NotFound, User } from "./sections";
+import { Affix, Layout } from 'antd';
+import { Viewer } from "./lib/types";
 import reportWebVitals from './reportWebVitals';
+import "./styles/index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
+const client = new ApolloClient({
+  uri: '/api', cache: new InMemoryCache()
+});
+
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false
+};
+
+const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  console.log(viewer);
+
+  return (
+    <Router>
+      <Layout id="app">
+        <Affix offsetTop={0} className="app__affix-header">
+          <AppHeader viewer={viewer} setViewer={setViewer} />
+        </Affix>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route exact path="/login" render={props => <Login {...props} setViewer={setViewer} />} />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </Router>
+  );
+};
+
+render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
