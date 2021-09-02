d4446843998bd0080853bbb36afaca97266228c1
scottkoons <scottkoons@gmail.com>
2021-09-02 13:57:32 -0600

    6-4 before check isviewer

--- src/sections/User/index.tsx
+++ src/sections/User/index.tsx
@@ -1,5 +1,50 @@
 import React from "react";
+import { RouteComponentProps, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { Col, Layout, Row } from "antd";
+import { USER } from "../../lib/graphql/queries";
+import {
+  User as UserData,
+  UserVariables
+} from "../../lib/graphql/queries/User/__generated__/User";
+import { Viewer } from "../../lib/types";
+import { UserProfile } from "./components";
 
-export const User = () => {
-  return <h2>User</h2>;
+
+interface Props {
+  viewer: Viewer;
+}
+
+
+interface MatchParams {
+  id: string;
+}
+
+const { Content } = Layout;
+
+export const User = ({ viewer }: Props) => {
+  const { id } = useParams<MatchParams>();
+
+  const { data, loading, error, refetch } = useQuery<UserData, UserVariables>(USER, {
+    variables: {
+      id
+    }
+  });
+
+  const user = data ? data.user : null;
+
+  const userProfileElement = user ? <UserProfile user={user} /> : null;
+
+  return (
+    <Content className="user">
+      <Row gutter={12} justify="space-between">
+        <Col xs={24}>{userProfileElement}</Col>
+      </Row>
+    </Content>
+
+    // <Content className="user">
+    //   <h2>{userProfileElement}</h2>
+    // </Content>
+
+  );
 };
