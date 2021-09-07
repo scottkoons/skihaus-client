220f417c0b54919aa82ced332fd6608cfa3a4d2f
scottkoons <scottkoons@gmail.com>
2021-09-07 15:14:31 -0600

    end mod 9 lesson 5

--- src/lib/graphql/queries/Listings/__generated__/Listings.ts
+++ src/lib/graphql/queries/Listings/__generated__/Listings.ts
@@ -20,6 +20,8 @@ export interface Listings_listings_result {
 
 export interface Listings_listings {
   __typename: "Listings";
+  region: string | null;
+  total: number;
   result: Listings_listings_result[];
 }
 
@@ -28,6 +30,7 @@ export interface Listings {
 }
 
 export interface ListingsVariables {
+  location?: string | null;
   filter: ListingsFilter;
   limit: number;
   page: number;
